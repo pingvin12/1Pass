e8f5304e5d023fc758ee2e3cf1900c79a3b08d1b
pingvin12 <fenyesj@gmail.com>
2022-12-03 21:49:36 +0100

    init tauri-react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { useState, useEffect } from 'react'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+  const Letters = [ 'R', 'O', 'D', 'E', 'O' ];
+  const moving = require('react-moving-text');
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => { setLoading(false) }, 99999999)
+  }, [])
+  return (<>
+  {
+    loading ?
+    Letters.map((letter, index) =>
+      <moving.MovingComponent
+        type="effect3D"
+        duration="2000ms"
+        delay="index * 100ms"
+        direction="reverse"
+        timing="ease"
+        iteration="infinite"
+        fillMode="both">
+        {letter}
+      </moving.MovingComponent>)
+  :
+  <Component {...pageProps} />
+  }
+  </>
+  )
 }

522f32495f40d5849dcaa9cc066331d5a75d10c3
pingvin12 <fenyesj@gmail.com>
2022-12-04 01:32:39 +0100

    Added loading screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import LoadingPage from '../Components/LoadingPage';
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
-  const Letters = [ 'R', 'O', 'D', 'E', 'O' ];
-  const moving = require('react-moving-text');
   useEffect(() => {
     setLoading(true)
-    setTimeout(() => { setLoading(false) }, 99999999)
+    setTimeout(() => { setLoading(false) }, 10000)
   }, [])
-  return (<>
+  return (
+  <>
   {
     loading ?
-    Letters.map((letter, index) =>
-      <moving.MovingComponent
-        type="effect3D"
-        duration="2000ms"
-        delay="index * 100ms"
-        direction="reverse"
-        timing="ease"
-        iteration="infinite"
-        fillMode="both">
-        {letter}
-      </moving.MovingComponent>)
+    <LoadingPage/>
   :
   <Component {...pageProps} />
   }

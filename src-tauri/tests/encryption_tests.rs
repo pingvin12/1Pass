ad7b240f2bff664113bccce2e0c1d3385a5ac067
pingvin12 <fenyesj@gmail.com>
2023-05-14 17:45:39 +0200

    add encryption tests

---
+++ src-tauri/tests/encryption_tests.rs
@@ -0,0 +1,29 @@
+use app::db::encryption;
+pub mod encryption_tests {
+    use app::db::encryption;
+
+
+    #[test]
+    pub fn encrypt_data_success() {
+       let data = encryption::encrypt_data("data".to_string());
+       assert_eq!(data.to_string(), "2Tr/he/Nmf5Ix4woroGM+g==".to_string());
+    }
+
+    #[test]
+    pub fn encrypt_data_fail() {
+        let data = encryption::encrypt_data("data".to_string());
+        assert_ne!(data.to_string(), "wrong".to_string());     
+    }
+
+    #[test]
+    pub fn decrypt_data_success() {
+        let data = encryption::decrypt_data("2Tr/he/Nmf5Ix4woroGM+g==".to_string());
+        assert_eq!(data.to_string(), "data".to_string());
+    }
+
+    #[test]
+    pub fn decrypt_data_fail() {
+        let data = encryption::decrypt_data("2Tr/he/Nmf5Ix4woroGM+g!==".to_string());
+        assert_ne!(data.to_string(), "wrong".to_string());
+    }
+}

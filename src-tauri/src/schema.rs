83adf0fe58c07d0c730512ab185008e9d6a94c11
pingvin12 <fenyesj@gmail.com>
2023-06-06 23:10:48 +0200

    feat(secrets): add migration

--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -1,6 +1,16 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    secrets (id) {
+        id -> Int4,
+        uid -> Nullable<Int4>,
+        name -> Varchar,
+        value -> Text,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
@@ -9,3 +19,8 @@ diesel::table! {
         created_at -> Timestamp,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    secrets,
+    users,
+);

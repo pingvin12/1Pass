7544a2eee04c1503ea134a7df92e9bf65d022145
pingvin12 <fenyesj@gmail.com>
2023-06-16 01:54:53 +0200

    chore(build): fix build error

--- next.config.js
+++ next.config.js
@@ -3,14 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+    unoptimized: true,
   },
-  experimental: {
-    appDir: true,
-  },
-  port: 4200,
-  distDir: "./out",
-  output: "export",
 };
 
 module.exports = nextConfig;

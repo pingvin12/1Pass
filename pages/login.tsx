a733af3f805d54c19440143a249e1427491e836f
pingvin12 <fenyesj@gmail.com>
2023-06-15 02:49:44 +0200

    feat(auth): auth on navbar, rewrite

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import SimpleReactValidator from "simple-react-validator";
 import { invoke } from "@tauri-apps/api/tauri";
+import { authObject } from "./api/models/auth.object";
+import dynamic from "next/dynamic";
+import { loginAsync } from "./api/auth/auth";
+import router from "next/router";
+import { useSWRConfig } from "swr";
 
 interface LoginProps {
   validator: SimpleReactValidator;
@@ -19,36 +24,29 @@ export default function Login() {
   };
 
   useEffect(() => {
-    const completeForm = async () => {
-      if (typeof window !== "undefined") {
-        if (validator.allValid()) {
-          try {
-            const token: string = await invoke("command_login_user", {
-              email,
-              password,
-            });
-            console.log(token);
-          } catch (error) {
-            console.error(error);
-          }
-        } else {
-          validator.showMessages();
-          forceUpdate();
+    if (typeof window !== "undefined") {
+      if (validator.allValid() && login) {
+        try {
+          const data = loginAsync(email, password);
+          console.log(data);
+          router.push("home");
+        } catch (error) {
+          console.error("Login page error", error);
         }
+      } else {
+        validator.showMessages();
+        forceUpdate();
       }
-    };
-
-    completeForm();
+    }
   }, [login]);
 
   return (
-    <div className="rounded-xl bg-white bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-      <div className="text-white">
-        <div className="mb-8 flex flex-col items-center">
-          <h1 className="mb-2 text-2xl">1Pass</h1>
-          <span className="text-gray-300">Login</span>
+    <div className="login-container-container">
+      <div className="login-container">
+        <div className="login-span-header-container">
+          <span className="login-span-header">Login</span>
         </div>
-        <div className="mb-4 text-lg">
+        <div className="login-email-error-container">
           <input
             type="text"
             name="Email"
@@ -57,7 +55,7 @@ export default function Login() {
           />
           {validator.message("email", email, "required")}
         </div>
-        <div className="mb-4 text-lg">
+        <div className="login-password-error-container">
           <input
             type="password"
             name="Password"
@@ -66,7 +64,7 @@ export default function Login() {
           />
           {validator.message("password", password, "required")}
         </div>
-        <div className="mt-8 flex justify-center text-lg text-black">
+        <div className="login-btn-container">
           <button onClick={(e) => loginClicked(true)}>Login</button>
         </div>
       </div>
--- pages/register.tsx
+++ pages/register.tsx
@@ -2,32 +2,39 @@ import Head from "next/head";
 import Image from "next/image";
 import SimpleReactValidator from "simple-react-validator";
 import { useRouter } from "next/router";
-// When using the Tauri API npm package:
-import { invoke } from "@tauri-apps/api/tauri";
-import { useState } from "react";
-// When using the Tauri global script (if not using the npm package)
-// Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
+import { useEffect, useState } from "react";
+import "./api/auth/auth";
+import { registerAsync } from "./api/auth/auth";
 export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
   const validator = new SimpleReactValidator();
-  function completeForm() {
-    console.log(validator.allValid());
-    if (validator.allValid()) {
-      invoke("command_register_user", {
-        username: username,
-        password: password,
-        email: email,
-      }).then(() => {
-        router.push("misc/completedRegistration");
-      });
-    } else {
-      forceUpdate();
-      validator.showMessages();
-    }
-  }
+  const [register, registerClicked] = useState(false);
+
+  useEffect(() => {
+    const completeForm = async () => {
+      if (typeof window !== "undefined") {
+        if (validator.allValid()) {
+          try {
+            const data = registerAsync(email, username, password);
+            data.then((val) => {
+              router.push("misc/completedRegistration");
+              return val;
+            });
+          } catch (error) {
+            console.error(error);
+          }
+        } else {
+          validator.showMessages();
+          forceUpdate();
+        }
+      }
+    };
+
+    completeForm();
+  }, [register]);
 
   const forceUpdate = () => {
     setUsername(username);
@@ -37,23 +44,22 @@ export default function Register() {
   };
 
   return (
-    <div className="rounded-xl bg-white bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-      <div className="text-white">
-        <div className="mb-8 flex flex-col items-center">
-          <h1 className="mb-2 text-2xl">1Pass</h1>
-          <span className="text-gray-300">Register</span>
+    <div className="register-container-container">
+      <div className="register-container">
+        <div className="register-header-container">
+          <span className="register-header">Register</span>
         </div>
-        <div className="mb-4 text-lg">
+        <div className="register-input-container">
           <input
             type="text"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="youremail@email.com"
+            placeholder="youremail@example.com"
           />
           {validator.message("Email", email, "required|email")}
         </div>
-        <div className="mb-4 text-lg">
+        <div className="register-username-container">
           <input
             type="text"
             name="username"
@@ -63,7 +69,7 @@ export default function Register() {
           />
           {validator.message("Username", username, "required")}
         </div>
-        <div className="mb-4 text-lg">
+        <div className="register-password-container">
           <input
             type="password"
             name="password"
@@ -73,8 +79,8 @@ export default function Register() {
           />
           {validator.message("Password", password, "required|min:8")}
         </div>
-        <div className="mt-8 flex justify-center text-lg text-black">
-          <button onClick={() => completeForm()}>Register</button>
+        <div className="register-button-container">
+          <button onClick={(e) => registerClicked(true)}>Register</button>
         </div>
       </div>
     </div>
